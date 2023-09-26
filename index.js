9d1700a1ee0f1efbbaee66993ec45b9b9a6d8956
Abdulhafiz <abdulhafizaderemi@gmail.com>
2023-09-26 09:27:55 +0100

    second commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", (msg) => {
     // console.log(`message: ${msg}`);
-    io.emit("chat message", `${msg});
+    io.emit("chat message", `${msg}`);
   });
 
   // socket.on("disconnect", () => {
